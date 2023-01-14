1a07694a863f66b891f68adbd7f15afa33556ba8
Vu Pham <vup.pham@terralogic.com>
2023-01-14 10:25:57 +0700

    Clean stuff

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React from "react";
-import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-import Main from "./Components/Main";
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="demo-big-content">
-      <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>} scroll>
-          <Navigation>
-            <Link to="/Resume">Resume</Link>
-            <Link to="/Aboutme">About Me</Link>
-            <Link to="/Projects">Projects</Link>
-            <Link to="/Contact">Contact</Link>
-          </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Portfolio</Link>}>
-          <Navigation>
-            <Link to="/Resume">Resume</Link>
-            <Link to="/Aboutme">About Me</Link>
-            <Link to="/Projects">Projects</Link>
-            <Link to="/Contact">Contact</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className="page-content"></div>
-          <Main />
-        </Content>
-      </Layout>
+      <h1>HELLO WOULD</h1>
     </div>
   );
 }
